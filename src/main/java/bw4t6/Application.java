260dc999e8b0e373977e4ff0d7415e438c729825
CristianMartu <martuccicristian18@gmail.com>
2024-06-10 11:53:11 +0200

    add database

--- src/main/java/bw4t6/Application.java
+++ src/main/java/bw4t6/Application.java
@@ -1,6 +1,10 @@
 package bw4t6;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("team_database");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
